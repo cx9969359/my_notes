80de0c85447f21cbad8bcab7e8d0c69522fc2e04
Charlie.chang <1929969359@qq.com>
2018-10-08 18:19:28 +0800

    【修改】

--- daily_poster.py
+++ daily_poster.py
@@ -6,14 +6,13 @@ import sys, os
 
 class Test():
     def test(self):
-        title = '声德练就好声音'
-        title_font = ImageFont.truetype(font_medium_type, 40)
+        title = '声德·练就好声音'
         # 打开图片
         image = Image.open(r'C:\Users\Administrator\Desktop\background.png')
         # 画图
         draw = ImageDraw.Draw(image)
         # 标题
-        draw.text((20, 10), title, font=title_font)
+        draw.text((20, 10), title)
         # 内容框
         draw.polygon([(20, 240), (350, 240), (350, 400), (20, 400)], fill='#ffffff')
         draw.polygon([(20, 440), (350, 440), (350, 600), (20, 600)], fill='#ffffff')
